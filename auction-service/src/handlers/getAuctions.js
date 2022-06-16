7619fd3307c17ae4e8266d09a9cde4480f2e6c32
gtinoco <genaro_13@hotmail.es>
2022-06-15 23:03:47 -0600

    Filtering auctions by status

--- auction-service/src/handlers/getAuctions.js
+++ auction-service/src/handlers/getAuctions.js
@@ -6,9 +6,21 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, _context) {
     let auctions;
+    const {status = 'OPEN'} = event.queryStringParameters;
+    const params = {
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        IndexName: 'statusAndEndDate',
+        KeyConditionExpression: '#status = :status',
+        ExpressionAttributeValues: {
+            ':status': status
+        }, ExpressionAttributeNames: {
+            '#status': 'status'
+        }
+    };
 
     try {
-        const results = await dynamodb.scan({TableName: process.env.AUCTIONS_TABLE_NAME,}).promise();
+        // const results = await dynamodb.scan({TableName: process.env.AUCTIONS_TABLE_NAME,}).promise();
+        const results = await dynamodb.query(params).promise();
 
         auctions = results.Items;
     } catch (error) {
